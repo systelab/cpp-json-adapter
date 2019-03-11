8c7a7708c1bd75e616eda0ba93e2d0355e096d02
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-11 23:46:12 +0100

    Added conan recipie for JSON adapter

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, tools
+
+
+class JSONAdapterConan(ConanFile):
+    name = "JSONAdapter"
+    description = "Library-agnostic API for C++ to work with JSON documents"
+    url = "https://github.com/systelab/cpp-json-adapter"
+    homepage = "https://github.com/systelab/cpp-json-adapter"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "json", "adapter", "wrapper")	
+    license = "MIT"
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("TestUtilitiesInterface/1.0.0@systelab/stable")
+
+    def package(self):
+        self.copy("*.h", dst="include/JSONAdapterInterface", src="JSONAdapterInterface")
+        self.copy("*.h", dst="include/JSONAdapterTestUtilities", src="JSONAdapterTestUtilities")
+        self.copy("*TestUtilitiesJSONAdapter.lib", dst="lib", keep_path=False)
+        self.copy("*TestUtilitiesJSONAdapter.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
