09d7f5b71ca1bf38e58b49e9161d58faa9921856
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-04-04 15:07:27 +0200

    Added test for conan package

---
+++ test_package/JSONAdapterExample.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+#include "JSONAdapterTestUtilities/Mocks/MockJSONAdapter.h"
+
+
+int main(int argc, char *argv[])
+{
+    systelab::json::test_utility::MockJSONAdapter jsonAdapter;
+    std::cout << "JSON adapter test utilities work as expected" << std::endl;
+
+    return 0;
+}
