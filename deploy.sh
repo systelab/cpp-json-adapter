4d4be3f228e9bdbc458bc8038ae44bc7e31fdf43
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-22 15:08:14 +0200

    Performed deploy using a bash script

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function checkErrors
+{
+	if [ $? != 0 ]
+	then
+		echo 
+		echo "An error occured while deploying artifacts."
+		echo
+		exit 1
+	fi
+}
+
+function parseParameters
+{
+	VERSION="None"
+	while getopts "hv:" OPTION
+	do
+		case $OPTION in
+		h)
+			usage #help: show usage section
+			exit 0
+			;;
+		v)
+			VERSION=$OPTARG
+			;;
+		?)
+			echo "Invalid parameter" 
+			usage
+			exit 1
+			;;
+		esac
+	done
+	
+	echo "********** DEPLOY PARAMETERS **********"
+	echo "VERSION: $VERSION"
+}
+
+parseParameters "${@}"
+
+echo -e "\nUploading JSONAdapterInterface/$VERSION@systelab/stable to systelab-bintray...\n\n"
+conan upload "JSONAdapterInterface/$VERSION@systelab/stable" --all -r systelab-bintray --force
+checkErrors
+
+echo -e "\nUploading JSONAdapterTestUtilities/$VERSION@systelab/stable to systelab-bintray...\n\n"
+conan upload "JSONAdapterTestUtilities/$VERSION@systelab/stable" --all -r systelab-bintray --force
+checkErrors
