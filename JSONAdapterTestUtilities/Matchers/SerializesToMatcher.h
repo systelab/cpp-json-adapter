49d2fb0ef24c77d1de303a33cd0d26c7c9a2baa6
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-09 10:16:25 +0100

    Added matcher for JSON arguments of function calls

---
+++ JSONAdapterTestUtilities/Matchers/SerializesToMatcher.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "JSONAdapterUtilities.h"
+#include "TestUtilitiesInterface/EntityComparator.h"
+
+
+namespace systelab { namespace json { namespace test_utility {
+
+	MATCHER_P(serializesTo, expectedJSON, jsonAdapter, "JSONAdapterComparison") \
+	{ \
+		AssertionResult rc = compareJSONs(expectedJSON, arg, jsonAdapter); \
+		*result_listener << rc.message(); \
+		return rc; \
+	};
+
+}}}
