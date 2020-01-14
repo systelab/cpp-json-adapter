acdc0b18b2b9fe23a77efdaf97af61821cbbb26d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-01-14 22:24:08 +0100

    Prepared for use of gtest 1.10.0

--- JSONAdapterTestUtilities/conanfile.py
+++ JSONAdapterTestUtilities/conanfile.py
@@ -22,7 +22,7 @@ class JSONAdapterTestUtilitiesConan(ConanFile):
         elif self.options.gtest == "1.8.1":
             self.requires("gtest/1.8.1@bincrafters/stable")
         else:
-            self.requires("gtest/1.10.0")
+            self.requires("gtest/1.10.0@systelab/stable")
 
         self.requires("TestUtilitiesInterface/1.0.3@systelab/stable")
         if ("%s" % self.version) == "None":
